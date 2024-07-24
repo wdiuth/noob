7356b0df50e3284fa5d57528f29892f80602b47f
wdiuth <95620522+wdiuth@users.noreply.github.com>
2024-07-24 13:13:58 +0330

    Add files via upload

---
+++ Untitled-1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+main()
+{
+	int A,B;
+	cout<<"enter A:";
+    cin>>A;
+    cout<<"enter B:";
+    cin>>B;
+   cout<<A+B;
+   getch();
+}
